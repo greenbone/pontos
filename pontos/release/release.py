66ad9c75476e88aa705d5e2b0545ac7a7ddf335d
Björn Ricks <bjoern.ricks@greenbone.net>
2021-04-15 13:47:28 +0200

    Refactor release modules
    Update the release module to be more precise about which parameters the
    commands prepare, release and sign require. This will simplify using
    pontos release.

--- pontos/release/release.py
+++ pontos/release/release.py
@@ -25,38 +25,13 @@ import json
 import shutil
 
 from pathlib import Path
-from typing import Callable, Dict, List, Union, Tuple, Optional
-from dataclasses import dataclass
+from typing import Callable, Dict, List, Union, Tuple
 
 import requests
+from requests.api import request
 
 from pontos import version
 from pontos import changelog
-
-
-@dataclass
-class ReleaseInformation:
-    release_version: str
-    nextversion: str
-    project: str
-    space: str
-    git_signing_key: str
-    git_tag_prefix: str
-    git_remote_name: str
-    username: str
-    token: str
-    signing_key: Optional[str]
-    path: Path
-    requests_module: requests
-    version_module: version
-    changelog_module: changelog
-    shell_cmd_runner: Callable
-
-    def git_version(self):
-        return "{}{}".format(self.git_tag_prefix, self.release_version)
-
-    def auth(self):
-        return (self.username, self.token)
 
 
 def build_release_dict(
@@ -93,16 +68,6 @@ def initialize_default_parser() -> argparse.ArgumentParser:
         prog='pontos-release',
     )
     parser.add_argument(
-        '--release-version',
-        help='Will release changelog as version. Must be PEP 440 compliant',
-        required=True,
-    )
-    parser.add_argument(
-        '--next-release-version',
-        help='Sets the next PEP 440 compliant version in project definition.',
-        required=True,
-    )
-    parser.add_argument(
         '--project',
         help='The github project',
         required=True,
@@ -112,19 +77,6 @@ def initialize_default_parser() -> argparse.ArgumentParser:
         default='greenbone',
         help='user/team name in github',
     )
-    parser.add_argument(
-        '--signing-key',
-        default='0ED1E580',
-        help='The key to sign zip, tarballs of a release. Default %(default)s.',
-    )
-    parser.add_argument(
-        '--git-signing-key',
-        help='The key to sign the commits and tag for a release',
-    )
-    parser.add_argument(
-        '--git-remote-name',
-        help='The git remote name to push the commits and tag to',
-    )
 
     subparsers = parser.add_subparsers(
         title='subcommands',
@@ -133,13 +85,67 @@ def initialize_default_parser() -> argparse.ArgumentParser:
         dest='command',
     )
 
-    subparsers.add_parser('prepare')
-    subparsers.add_parser('release')
-    subparsers.add_parser('sign')
+    prepare_parser = subparsers.add_parser('prepare')
+    prepare_parser.set_defaults(func=prepare)
+    prepare_parser.add_argument(
+        '--release-version',
+        help='Will release changelog as version. Must be PEP 440 compliant',
+        required=True,
+    )
+    prepare_parser.add_argument(
+        '--next-version',
+        help='Sets the next PEP 440 compliant version in project definition '
+        'after the release',
+        required=True,
+    )
+    prepare_parser.add_argument(
+        '--git-signing-key',
+        help='The key to sign the commits and tag for a release',
+    )
+    prepare_parser.add_argument(
+        '--git-tag-prefix',
+        default='v',
+        help='Prefix for git tag versions. Default: %(default)s',
+    )
+
+    release_parser = subparsers.add_parser('release')
+    release_parser.set_defaults(func=release)
+    release_parser.add_argument(
+        '--release-version',
+        help='Will release changelog as version. Must be PEP 440 compliant',
+        required=True,
+    )
+    release_parser.add_argument(
+        '--git-remote-name',
+        help='The git remote name to push the commits and tag to',
+    )
+    release_parser.add_argument(
+        '--git-tag-prefix',
+        default='v',
+        help='Prefix for git tag versions. Default: %(default)s',
+    )
+
+    sign_parser = subparsers.add_parser('sign')
+    sign_parser.set_defaults(func=sign)
+    sign_parser.add_argument(
+        '--signing-key',
+        default='0ED1E580',
+        help='The key to sign zip, tarballs of a release. Default %(default)s.',
+    )
+    sign_parser.add_argument(
+        '--release-version',
+        help='Will release changelog as version. Must be PEP 440 compliant',
+        required=True,
+    )
+    sign_parser.add_argument(
+        '--git-tag-prefix',
+        default='v',
+        help='Prefix for git tag versions. Default: %(default)s',
+    )
     return parser
 
 
-def parse(args=None):
+def parse(args=None) -> Tuple[str, str, argparse.Namespace]:
     parser = initialize_default_parser()
     commandline_arguments = parser.parse_args(args)
     token = (
@@ -152,18 +158,7 @@ def parse(args=None):
         if not args or not 'testcases' in args
         else 'USER'
     )
-    return (
-        commandline_arguments.command,
-        commandline_arguments.release_version,
-        commandline_arguments.next_release_version,
-        commandline_arguments.project,
-        commandline_arguments.space,
-        commandline_arguments.signing_key,
-        commandline_arguments.git_signing_key,
-        commandline_arguments.git_remote_name,
-        user,
-        token,
-    )
+    return (user, token, commandline_arguments)
 
 
 def update_version(
@@ -199,35 +194,85 @@ def commit_files(
     )
 
 
+def upload_assets(
+    username: str,
+    token: str,
+    pathnames: List[str],
+    github_json: Dict,
+    path: Path,
+    requests_module: request,
+) -> bool:
+    print("Uploading assets: {}".format(pathnames))
+
+    asset_url = github_json['upload_url'].replace('{?name,label}', '')
+    paths = [path('{}.asc'.format(p)) for p in pathnames]
+
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'content-type': 'application/octet-stream',
+    }
+    auth = (username, token)
+
+    for path in paths:
+        to_upload = path.read_bytes()
+        resp = requests_module.post(
+            "{}?name={}".format(asset_url, path.name),
+            headers=headers,
+            auth=auth,
+            data=to_upload,
+        )
+
+        if resp.status_code != 201:
+            print(
+                "Wrong response status {} while uploading {}".format(
+                    resp.status_code, path.name
+                )
+            )
+            print(json.dumps(resp.text, indent=4, sort_keys=True))
+            return False
+        else:
+            print("uploaded: {}".format(path.name))
+
+    return True
+
+
 def prepare(
-    release_info: ReleaseInformation,
+    shell_cmd_runner: Callable,
+    args: argparse.Namespace,
+    *,
+    path: Path,
+    version_module: version,
+    changelog_module: changelog,
+    **_kwargs,
 ):
+    project: str = args.project
+    space: str = args.space
+    git_tag_prefix: str = args.git_tag_prefix
+    git_signing_key: str = args.git_signing_key
+    release_version: str = args.release_version
+    next_version: str = args.next_version
+
     print("in prepare")
+
     # guardian
-    git_tags = release_info.shell_cmd_runner('git tag -l')
-    git_version = "{}{}".format(
-        release_info.git_tag_prefix, release_info.release_version
-    )
+    git_tags = shell_cmd_runner('git tag -l')
+    git_version = "{}{}".format(git_tag_prefix, release_version)
     if git_version.encode() in git_tags.stdout.splitlines():
         raise ValueError('git tag {} is already taken.'.format(git_version))
 
     executed, filename = update_version(
-        release_info.release_version, release_info.version_module, develop=False
+        release_version, version_module, develop=False
     )
     if not executed:
         return False
 
-    print(
-        "updated version {} to {}".format(
-            filename, release_info.release_version
-        )
-    )
+    print("updated version {} to {}".format(filename, release_version))
 
-    change_log_path = release_info.path.cwd() / 'CHANGELOG.md'
-    updated, changelog_text = release_info.changelog_module.update(
+    change_log_path = path.cwd() / 'CHANGELOG.md'
+    updated, changelog_text = changelog_module.update(
         change_log_path.read_text(),
-        release_info.release_version,
-        git_tag_prefix=release_info.git_tag_prefix,
+        release_version,
+        git_tag_prefix=git_tag_prefix,
     )
 
     if not updated:
@@ -235,62 +280,62 @@ def prepare(
 
     change_log_path.write_text(updated)
 
-    print("updated CHANGELOG.md")
+    print("Updated CHANGELOG.md")
 
     print("Committing changes")
 
-    commit_msg = 'automatic release to {}'.format(release_info.release_version)
+    commit_msg = 'automatic release to {}'.format(release_version)
     commit_files(
         filename,
         commit_msg,
-        release_info.git_signing_key,
-        release_info.shell_cmd_runner,
+        git_signing_key,
+        shell_cmd_runner,
     )
 
-    if release_info.git_signing_key:
-        release_info.shell_cmd_runner(
+    if git_signing_key:
+        shell_cmd_runner(
             "git tag -u {} {} -m '{}'".format(
-                release_info.git_signing_key, git_version, commit_msg
+                git_signing_key, git_version, commit_msg
             ),
         )
     else:
-        release_info.shell_cmd_runner(
+        shell_cmd_runner(
             "git tag -s {} -m '{}'".format(git_version, commit_msg),
         )
 
-    release_text = release_info.path(".release.txt.md")
+    release_text = path(".release.txt.md")
     release_text.write_text(changelog_text)
 
     # set to new version add skeleton
     executed, filename = update_version(
-        release_info.nextversion, release_info.version_module, develop=True
+        next_version, version_module, develop=True
     )
     if not executed:
         return False
 
-    updated = release_info.changelog_module.add_skeleton(
+    updated = changelog_module.add_skeleton(
         change_log_path.read_text(),
-        release_info.release_version,
-        release_info.project,
-        git_tag_prefix=release_info.git_tag_prefix,
-        git_space=release_info.space,
+        release_version,
+        project,
+        git_tag_prefix=git_tag_prefix,
+        git_space=space,
     )
     change_log_path.write_text(updated)
 
-    commit_msg = 'set version {}, add empty changelog after {}'.format(
-        release_info.nextversion, release_info.release_version
+    commit_msg = (
+        f'* Update to version {next_version}\n'
+        f'* Add empty changelog after {release_version}'
     )
     commit_files(
         filename,
         commit_msg,
-        release_info.git_signing_key,
-        release_info.shell_cmd_runner,
+        git_signing_key,
+        shell_cmd_runner,
     )
 
     print(
-        "Please verify git tag {}, commit and release text in {}".format(
-            git_version, release_text
-        )
+        f"Please verify git tag {git_version}, "
+        f"commit and release text in {str(release_text)}"
     )
     print("Afterwards please execute release")
 
@@ -298,36 +343,46 @@ def prepare(
 
 
 def release(
-    release_info: ReleaseInformation,
+    shell_cmd_runner: Callable,
+    args: argparse.Namespace,
+    *,
+    path: Path,
+    username: str,
+    token: str,
+    requests_module: requests,
+    **_kwargs,
 ):
-    changelog_text = release_info.path(".release.txt.md").read_text()
+    project: str = args.project
+    space: str = args.space
+    git_remote_name: str = args.git_remote_name
+    git_tag_prefix: str = args.git_tag_prefix
+    release_version: str = args.release_version
+
+    changelog_text: str = path(".release.txt.md").read_text()
 
     print("Pushing changes")
 
-    if release_info.git_remote_name:
-        release_info.shell_cmd_runner(
-            "git push --follow-tags {}".format(release_info.git_remote_name)
-        )
+    if git_remote_name:
+        shell_cmd_runner("git push --follow-tags {}".format(git_remote_name))
     else:
-        release_info.shell_cmd_runner("git push --follow-tags")
+        shell_cmd_runner("git push --follow-tags")
 
     print("Creating release")
 
     headers = {'Accept': 'application/vnd.github.v3+json'}
 
     base_url = "https://api.github.com/repos/{}/{}/releases".format(
-        release_info.space, release_info.project
+        space, project
     )
-    response = release_info.requests_module.post(
+    git_version = f'{git_tag_prefix}{release_version}'
+    response = requests_module.post(
         base_url,
         headers=headers,
-        auth=release_info.auth(),
+        auth=(username, token),
         json=build_release_dict(
-            release_info.git_version(),
+            git_version,
             changelog_text,
-            name="{} {}".format(
-                release_info.project, release_info.release_version
-            ),
+            name="{} {}".format(project, release_version),
         ),
     )
     if response.status_code != 201:
@@ -335,61 +390,46 @@ def release(
         print(json.dumps(response.text, indent=4, sort_keys=True))
         return False
 
-    release_info.path(".release.txt.md").unlink()
+    path(".release.txt.md").unlink()
     return True
 
 
-def sign(release_info: ReleaseInformation):
-    def upload_assets(pathnames: List[str], github_json: Dict) -> bool:
-        print("Uploading assets: {}".format(pathnames))
-
-        asset_url = github_json['upload_url'].replace('{?name,label}', '')
-        paths = [release_info.path('{}.asc'.format(p)) for p in pathnames]
-
-        headers = {
-            'Accept': 'application/vnd.github.v3+json',
-            'content-type': 'application/octet-stream',
-        }
-
-        for path in paths:
-            to_upload = path.read_bytes()
-            resp = release_info.requests_module.post(
-                "{}?name={}".format(asset_url, path.name),
-                headers=headers,
-                auth=release_info.auth(),
-                data=to_upload,
-            )
-
-            if resp.status_code != 201:
-                print(
-                    "Wrong response status {} while uploading {}".format(
-                        resp.status_code, path.name
-                    )
-                )
-                print(json.dumps(resp.text, indent=4, sort_keys=True))
-                return False
-            else:
-                print("uploaded: {}".format(path.name))
-
-        return True
-
+def sign(
+    shell_cmd_runner: Callable,
+    args: argparse.Namespace,
+    *,
+    path: Path,
+    username: str,
+    token: str,
+    requests_module: requests,
+    **_kwargs,
+):
     def download(url, filename):
-        file_path = release_info.path("/tmp/{}".format(filename))
+        file_path = path(f"/tmp/{filename}")
 
-        with release_info.requests_module.get(
-            url, stream=True
-        ) as resp, file_path.open(mode='wb') as download_file:
+        with requests_module.get(url, stream=True) as resp, file_path.open(
+            mode='wb'
+        ) as download_file:
             shutil.copyfileobj(resp.raw, download_file)
 
         return file_path
 
+    project: str = args.project
+    space: str = args.space
+    git_tag_prefix: str = args.git_tag_prefix
+    release_version: str = args.release_version
+    signing_key: str = args.signing_key
+
     headers = {'Accept': 'application/vnd.github.v3+json'}
 
-    base_url = "https://api.github.com/repos/{}/{}/releases/tags/{}".format(
-        release_info.space, release_info.project, release_info.git_version()
+    git_version: str = f'{git_tag_prefix}{release_version}'
+
+    base_url = (
+        f"https://api.github.com/repos/{space}/{project}"
+        f"/releases/tags/{git_version}"
     )
 
-    response = release_info.requests_module.get(
+    response = requests_module.get(
         base_url,
         headers=headers,
     )
@@ -399,28 +439,26 @@ def sign(release_info: ReleaseInformation):
         return False
 
     github_json = json.loads(response.text)
-    zip_path = download(
-        github_json['zipball_url'], release_info.git_version() + ".zip"
-    )
-    tar_path = download(
-        github_json['tarball_url'], release_info.git_version() + ".tar.gz"
-    )
+    zip_path = download(github_json['zipball_url'], git_version + ".zip")
+    tar_path = download(github_json['tarball_url'], git_version + ".tar.gz")
 
     print("Signing {}".format([zip_path, tar_path]))
 
     gpg_cmd = "gpg --default-key {} --detach-sign --armor {}"
-    release_info.shell_cmd_runner(
-        gpg_cmd.format(release_info.signing_key, zip_path)
-    )
-    release_info.shell_cmd_runner(
-        gpg_cmd.format(release_info.signing_key, tar_path)
-    )
+    shell_cmd_runner(gpg_cmd.format(signing_key, zip_path))
+    shell_cmd_runner(gpg_cmd.format(signing_key, tar_path))
 
-    return upload_assets([zip_path, tar_path], github_json)
+    return upload_assets(
+        username,
+        token,
+        [zip_path, tar_path],
+        github_json,
+        path=path,
+        requests_module=requests_module,
+    )
 
 
 def main(
-    git_tag_prefix: str = "v",
     shell_cmd_runner=lambda x: subprocess.run(
         x,
         shell=True,
@@ -436,54 +474,19 @@ def main(
     leave: bool = True,
     args=None,
 ):
-    def execute(
-        command: str,
-        release_version: str,
-        next_release_version: str,
-        project: str,
-        space: str,
-        signing_key: str,
-        git_signing_key: str,
-        git_remote_name: str,
-        user: str,
-        token: str,
-    ):
-        info = ReleaseInformation(
-            release_version=release_version,
-            nextversion=next_release_version,
-            project=project,
-            space=space,
-            git_signing_key=git_signing_key,
-            git_tag_prefix=git_tag_prefix,
-            git_remote_name=git_remote_name,
-            username=user,
-            token=token,
-            signing_key=signing_key,
-            path=_path,
-            requests_module=_requests,
-            version_module=_version,
-            changelog_module=_changelog,
-            shell_cmd_runner=shell_cmd_runner,
-        )
-
-        if command == 'prepare':
-            return prepare(info)
-        if command == 'release':
-            print("command release")
-            return release(
-                info,
-            )
-        if command == 'sign':
-            return sign(info)
-
-        raise ValueError("Unknown command: {}".format(command))
-
-    values = parse(args)
-    if not values:
-        return sys.exit(1) if leave else False
+    username, token, parsed_args = parse(args)
 
     try:
-        if not execute(*values):
+        if not parsed_args.func(
+            shell_cmd_runner,
+            parsed_args,
+            path=_path,
+            username=username,
+            token=token,
+            changelog_module=_changelog,
+            requests_module=_requests,
+            version_module=_version,
+        ):
             return sys.exit(1) if leave else False
     except subprocess.CalledProcessError as e:
         print(
