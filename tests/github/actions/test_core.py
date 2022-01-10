8a3ced17d84275d7da95bf0fd13e9f5cb2b73355
Björn Ricks <bjoern.ricks@greenbone.net>
2022-01-10 10:07:26 +0100

    Add: New GitHub API for supporting actions
    Add new GitHub Actions core API for IO.

---
+++ tests/github/actions/test_core.py
@@ -0,0 +1,111 @@
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from unittest.mock import MagicMock, patch
+
+from pontos.github.actions.core import ActionIO, Console
+
+
+@patch("builtins.print")
+class ConsoleTestCase(unittest.TestCase):
+    def test_start_group(self, print_mock):
+        Console.start_group("Foo")
+        print_mock.assert_called_once_with("::group::Foo")
+
+    def test_end_group(self, print_mock):
+        Console.end_group()
+        print_mock.assert_called_once_with("::endgroup::")
+
+    def test_group(self, print_mock):
+        with Console.group("Foo"):
+            print_mock.assert_called_once_with("::group::Foo")
+
+        print_mock.assert_called_with("::endgroup::")
+
+    def test_warning(self, print_mock):
+        Console.warning(
+            "foo",
+            name="bar",
+            line="123",
+            end_line="234",
+            column="1",
+            end_column="2",
+            title="Foo Bar",
+        )
+        print_mock.assert_called_once_with(
+            "::warning file=bar,line=123,endLine=234,col=1,endColumn=2,title=Foo Bar::foo"  # pylint: disable=line-too-long
+        )
+
+    def test_error(self, print_mock):
+        Console.error(
+            "foo",
+            name="bar",
+            line="123",
+            end_line="234",
+            column="1",
+            end_column="2",
+            title="Foo Bar",
+        )
+        print_mock.assert_called_once_with(
+            "::error file=bar,line=123,endLine=234,col=1,endColumn=2,title=Foo Bar::foo"  # pylint: disable=line-too-long
+        )
+
+    def test_notice(self, print_mock):
+        Console.notice(
+            "foo",
+            name="bar",
+            line="123",
+            end_line="234",
+            column="1",
+            end_column="2",
+            title="Foo Bar",
+        )
+        print_mock.assert_called_once_with(
+            "::notice file=bar,line=123,endLine=234,col=1,endColumn=2,title=Foo Bar::foo"  # pylint: disable=line-too-long
+        )
+
+    def test_log(self, print_mock):
+        Console.log("foo")
+
+        print_mock.assert_called_once_with("foo")
+
+    def test_debug(self, print_mock):
+        Console.debug("foo")
+
+        print_mock.assert_called_once_with("::debug::foo")
+
+
+class ActionIOTestCase(unittest.TestCase):
+    @patch.dict(
+        'os.environ', {"INPUT_FOO": "1234", "INPUT_FOO_BAR": "2345"}, clear=True
+    )
+    def test_input(self):
+        self.assertEqual(ActionIO.input("foo"), "1234")
+        self.assertEqual(ActionIO.input("FOO"), "1234")
+        self.assertEqual(ActionIO.input("FoO"), "1234")
+
+        self.assertEqual(ActionIO.input("foo bar"), "2345")
+        self.assertEqual(ActionIO.input("FOO_BAR"), "2345")
+        self.assertEqual(ActionIO.input("FoO BaR"), "2345")
+
+    @patch("builtins.print")
+    def test_output(self, print_mock: MagicMock):
+        ActionIO.output("foo", "bar")
+
+        print_mock.assert_called_once_with("::set-output name=foo::bar")
