ba95fb392cbaeb58c237f38b59cb1b8cd25c538e
Jaspar Löchte <jaspar.loechte@greenbone.net>
2022-01-31 14:27:24 +0100

    Add get modified and added files within a PR

--- pontos/github/api.py
+++ pontos/github/api.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from pathlib import Path
-from typing import Callable, Dict, Iterator, Optional
+from typing import Callable, Dict, Iterator, List, Optional
 
 import requests
 
@@ -332,3 +332,57 @@ class GitHubRESTApi:
         """
         api = f"https://github.com/{repo}/archive/refs/tags/{tag}.zip"
         return download(api, destination)
+
+    def get_modified_files_in_pr(self, repo: str, pr_number: int) -> List[Path]:
+        """
+        Get all modified files of a pull request
+
+        Hint: At maximum GitHub allows to receive 100 commits. If a pull request
+        contains more then 100 commits only the first 100 are returned.
+
+        Args:
+            repo: GitHub repository (owner/name) to use
+            pr_number: Pull request number
+
+        Returns:
+            Information about the commits in the pull request as a dict
+        """
+        # per default github only shows 35 commits and at max it is only
+        # possible to receive 100
+        # might add the page parameter, to get the files 101-202 and so on
+        params = {"per_page": "100"}
+        api = f"/repos/{repo}/pulls/{str(pr_number)}/files"
+        response = self._request(api, params=params)
+
+        return [
+            Path(f['filename'])
+            for f in response.json()
+            if f['status'] == "modified"
+        ]
+
+    def get_added_files_in_pr(self, repo: str, pr_number: int) -> List[Path]:
+        """
+        Get all added files of a pull request
+
+        Hint: At maximum GitHub allows to receive 100 commits. If a pull request
+        contains more then 100 commits only the first 100 are returned.
+
+        Args:
+            repo: GitHub repository (owner/name) to use
+            pr_number: Pull request number
+
+        Returns:
+            Information about the commits in the pull request as a dict
+        """
+        # per default github only shows 35 commits and at max it is only
+        # possible to receive 100
+        # might add the page parameter, to get the files 101-202 and so on
+        params = {"per_page": "100"}
+        api = f"/repos/{repo}/pulls/{str(pr_number)}/files"
+        response = self._request(api, params=params)
+
+        return [
+            Path(f['filename'])
+            for f in response.json()
+            if f['status'] == "added"
+        ]
