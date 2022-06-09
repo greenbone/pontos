3ae83aac5ad32d9cf8f5ec4e0ec5df56871abb16
Björn Ricks <bjoern.ricks@greenbone.net>
2022-06-09 13:58:26 +0200

    Remove tests for output functions
    The output functions have been removed thus the tests are obsolete now.

--- tests/terminal/test_terminal.py
+++ tests/terminal/test_terminal.py
@@ -213,35 +213,5 @@ class TerminalTestCase(unittest.TestCase):
         self.assertEqual(len(ret), expected_len)
 
 
-class TerminalOutputApiTestCase(unittest.TestCase):
-    def setUp(self):
-        self.term = Mock(spec=Terminal)
-        _set_terminal(self.term)
-
-    def test_error(self):
-        error('foo bar')
-        self.term.error.assert_called_with('foo bar')
-
-    def test_fail(self):
-        fail('foo bar')
-        self.term.fail.assert_called_with('foo bar')
-
-    def test_info(self):
-        info('foo bar')
-        self.term.info.assert_called_with('foo bar')
-
-    def test_ok(self):
-        ok('foo bar')
-        self.term.ok.assert_called_with('foo bar')
-
-    def test_out(self):
-        out('foo bar')
-        self.term.print.assert_called_with('foo bar')
-
-    def test_warning(self):
-        warning('foo bar')
-        self.term.warning.assert_called_with('foo bar')
-
-
 if __name__ == '__main__':
     unittest.main()
