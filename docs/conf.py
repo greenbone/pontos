44540d56b9cccbb833807a4178945780bebbaeea
pascalholthaus <pascal.holthaus@greenbone.net>
2022-07-18 12:52:11 +0200

    Change: m2r2 to MyST

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
     'sphinx_autodoc_typehints',
-    'm2r2'
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -60,7 +60,7 @@ pygments_style = None
 #
 html_theme = 'alabaster'
 
-html_favicon = 'favicon.gif'
+html_favicon = 'favicon.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
