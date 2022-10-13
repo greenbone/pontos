dacffdc72dad40a18981c65b6296550fb2d265f5
pascalholthaus <pascal.holthaus@greenbone.net>
2022-10-13 05:16:50 +0200

    Fix: help description

--- pontos/github/argparser.py
+++ pontos/github/argparser.py
@@ -346,11 +346,11 @@ def parse_args(
         "-d",
         "--draft",
         action="store_true",
-        help="Create draf release.",
+        help="Create a draft release.",
     )
 
     create_re_parser.add_argument(
-        "-p", "--prerelease", action="store_true", help="Create pre-release."
+        "-p", "--prerelease", action="store_true", help="Create a pre-release."
     )
 
     parsed_args = parser.parse_args(args)
