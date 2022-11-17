67aaf37972279cfe27b7dcba65cedefcaa53a695
Björn Ricks <bjoern.ricks@greenbone.net>
2022-11-17 12:11:52 +0100

    Add: Add repository type to repositories github script

--- scripts/github/repositories.py
+++ scripts/github/repositories.py
@@ -15,18 +15,44 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+This script prints out all repositories existing in the space of the given
+organization
+"""
+
 from argparse import ArgumentParser, Namespace
 
 from pontos.github.api import GitHubAsyncRESTApi
+from pontos.github.api.helper import RepositoryType
 from pontos.github.models.organization import Repository
+
+
+def repository_type(value: str | RepositoryType) -> RepositoryType:
+    if isinstance(value, RepositoryType):
+        return value
+    return RepositoryType[value.upper()]
 
 
 def add_script_arguments(parser: ArgumentParser) -> None:
     parser.add_argument("organization")
 
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=repository_type,
+        help=f"What type of repositories should be printed? Choices: "
+        f"{', '.join([f.name for f in RepositoryType])}. Default: %(default)s",
+        default=RepositoryType.ALL.name,
+    )
+
 
 async def github_script(api: GitHubAsyncRESTApi, args: Namespace) -> int:
-    async for repo in api.organizations.get_repositories(args.organization):
-        print(Repository.from_dict(repo))
+    repo_count = 0
+    async for repo in api.organizations.get_repositories(
+        args.organization, repository_type=args.type
+    ):
+        print(Repository.from_dict(repo), "\n")
+        repo_count += 1
 
+    print(f"{repo_count} repositories.")
     return 0
