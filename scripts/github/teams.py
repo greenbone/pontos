a28ae7d63bfc4818dc455a1f64f8985ca28ea0ec
Björn Ricks <bjoern.ricks@greenbone.net>
2022-11-24 11:21:24 +0100

    Add: Add GitHub script to list teams of an organization

---
+++ scripts/github/teams.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This script prints out all teams existing in the space of the given
+organization
+"""
+
+from argparse import ArgumentParser, Namespace
+
+from rich.console import Console
+from rich.table import Table
+
+from pontos.github.api import GitHubAsyncRESTApi
+
+
+def add_script_arguments(parser: ArgumentParser) -> None:
+    parser.add_argument("organization")
+
+
+async def github_script(api: GitHubAsyncRESTApi, args: Namespace) -> int:
+    table = Table()
+    table.add_column("Name")
+    table.add_column("Description")
+    table.add_column("URL")
+    table.add_column("Privacy")
+
+    count = 0
+    async for team in api.teams.get_all(args.organization):
+        table.add_row(
+            team.name,
+            team.description,
+            team.html_url,
+            team.privacy.value,
+        )
+        count += 1
+
+    console = Console()
+    console.print(table)
+
+    print(f"{count} teams.")
+    return 0
