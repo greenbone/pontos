267c18d33b01faa174f681292cd00bf80dc530eb
Björn Ricks <bjoern.ricks@greenbone.net>
2022-11-24 11:17:10 +0100

    Add: Add MergeStrategy to pontos.git module and add `__all__`

--- pontos/git/__init__.py
+++ pontos/git/__init__.py
@@ -15,4 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .git import Git, GitError
+from .git import Git, GitError, MergeStrategy
+
+__all__ = ("Git", "GitError", "MergeStrategy")
