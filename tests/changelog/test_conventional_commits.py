47d150e614ebf82f45692e4334b5a427a94d3601
Björn Ricks <bjoern.ricks@greenbone.net>
2023-01-27 10:22:19 +0100

    Change: Improve having no changelog config for conventional commits
    Add default config settings to make the changelog config optional.
    Additonally raise a ChangelogBuilderError if a config file is passed and
    the config file doesn't exist. This is clearly a mistake by the user.

--- tests/changelog/test_conventional_commits.py
+++ tests/changelog/test_conventional_commits.py
@@ -240,6 +240,87 @@ All notable changes to this project will be documented in this file.
 
             self.assertIsNone(output_file)
 
+    def test_changelog_builder_config_file_not_exists(self):
+        terminal = MagicMock()
+
+        with temp_directory() as temp_dir, self.assertRaisesRegex(
+            ChangelogBuilderError,
+            r"Changelog Config file '.*\.toml' does not exist\.",
+        ):
+            ChangelogBuilder(
+                terminal=terminal,
+                current_version="0.0.1",
+                next_version="1.0.0",
+                space="foo",
+                project="bar",
+                config=temp_dir / "changelog.toml",
+            )
+
+    @patch("pontos.changelog.conventional_commits.Git", autospec=True)
+    def test_changelog_builder_with_default_changelog_config(
+        self, git_mock: MagicMock
+    ):
+        terminal = MagicMock()
+
+        today = datetime.today().strftime("%Y-%m-%d")
+
+        release_version = "0.0.2"
+        output = f"v{release_version}.md"
+
+        git_mock.return_value.list_tags.return_value = ["v0.0.1"]
+        git_mock.return_value.log.return_value = [
+            "1234567 Add: foo bar",
+            "8abcdef Add: bar baz",
+            "8abcd3f Add bar baz",
+            "8abcd3d Adding bar baz",
+            "1337abc Change: bar to baz",
+            "42a42a4 Remove: foo bar again",
+            "fedcba8 Test: bar baz testing",
+            "dead901 Refactor: bar baz ref",
+            "fedcba8 Fix: bar baz fixing",
+            "d0c4d0c Doc: bar baz documenting",
+        ]
+
+        changelog_builder = ChangelogBuilder(
+            terminal=terminal,
+            current_version="0.0.1",
+            next_version=release_version,
+            space="foo",
+            project="bar",
+            config=None,
+        )
+        expected_output = f"""# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [0.0.2] - {today}
+
+## Added
+* foo bar [1234567](https://github.com/foo/bar/commit/1234567)
+* bar baz [8abcdef](https://github.com/foo/bar/commit/8abcdef)
+
+## Removed
+* foo bar again [42a42a4](https://github.com/foo/bar/commit/42a42a4)
+
+## Changed
+* bar to baz [1337abc](https://github.com/foo/bar/commit/1337abc)
+
+## Bug Fixes
+* bar baz fixing [fedcba8](https://github.com/foo/bar/commit/fedcba8)
+
+[0.0.2]: https://github.com/foo/bar/compare/0.0.1...0.0.2"""
+
+        with temp_directory(change_into=True):
+            output_file = changelog_builder.create_changelog_file(output)
+            self.assertEqual(
+                output_file.read_text(encoding="utf-8"), expected_output
+            )
+
+        git_mock.return_value.list_tags.assert_called_with(sort=TagSort.VERSION)
+        git_mock.return_value.log.assert_called_with(
+            "v0.0.1..HEAD", oneline=True
+        )
+
 
 class ParseArgsTestCase(unittest.TestCase):
     def test_parse_args(self):
