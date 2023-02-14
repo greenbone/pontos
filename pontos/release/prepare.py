678b32410d8d7e245a565fac1b1ac04e2a27201d
Björn Ricks <bjoern.ricks@greenbone.net>
2023-02-14 09:38:34 +0100

    Change: Refactor release and prepare CLI
    Use classes for better separation of concerns while implementing the
    CLIs. With the new coude it should be easier to understand what's
    actually happening and it fixes a nasty issue with adding changed files
    to the release commits.
    
    For release also change to async GitHub API.

--- pontos/release/prepare.py
+++ pontos/release/prepare.py
@@ -16,24 +16,31 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from argparse import Namespace
-from enum import IntEnum
+from enum import Enum, IntEnum
 from pathlib import Path
+from typing import Optional
 
-from pontos import changelog
+from pontos.changelog.changelog import ChangelogError, update_changelog
+from pontos.changelog.conventional_commits import ChangelogBuilder
 from pontos.git import Git
 from pontos.terminal import Terminal
+from pontos.version import COMMANDS
+from pontos.version.commands import get_current_version
+from pontos.version.errors import VersionError
+from pontos.version.helper import (
+    calculate_calendar_version,
+    get_next_patch_version,
+)
+from pontos.version.version import VersionUpdate
 
 from .helper import (
-    calculate_calendar_version,
-    commit_files,
     find_signing_key,
     get_git_repository_name,
     get_last_release_version,
-    get_next_patch_version,
-    update_version,
 )
 
 RELEASE_TEXT_FILE = ".release.md"
+DEFAULT_CHANGELOG_FILE = "CHANGELOG.md"
 
 
 class PrepareReturnValue(IntEnum):
@@ -41,7 +48,219 @@ class PrepareReturnValue(IntEnum):
     NO_RELEASE_VERSION = 1
     ALREADY_TAKEN = 2
     UPDATE_VERSION_ERROR = 3
-    NO_UNRELEASED_CHANGELOG = 4
+    CHANGELOG_ERROR = 4
+
+
+class ReleaseType(Enum):
+    PATCH = "patch"
+    CALENDAR = "calendar"
+    VERSION = "version"
+    # for the future we need
+    # MAJOR = "major"
+    # MINOR = "minor"
+
+
+class PrepareCommand:
+    def __init__(self, terminal: Terminal) -> None:
+        self.git = Git()
+        self.terminal = terminal
+
+    def _get_release_version(
+        self, release_type: ReleaseType, release_version: Optional[str]
+    ) -> str:
+        if release_type == ReleaseType.CALENDAR:
+            return calculate_calendar_version(get_current_version())
+
+        if release_type == ReleaseType.PATCH:
+            return get_next_patch_version(get_current_version())
+
+        if not release_version:
+            raise VersionError(
+                "No release version provided. Either use a different release "
+                "strategy or provide a release version."
+            )
+        return release_version
+
+    def _has_tag(self, git_version: str) -> bool:
+        git_tags = self.git.list_tags()
+        return git_version in git_tags
+
+    def _update_version(self, release_version: str) -> VersionUpdate:
+        for cmd in COMMANDS:
+            command = cmd()
+            project_file = command.project_file_found()
+            if project_file:
+                return command.update_version(new_version=release_version)
+
+        raise VersionError("No project definition found.")
+
+    def _create_old_changelog(
+        self, release_version: str, changelog_file: Optional[Path]
+    ) -> str:
+        if not changelog_file:
+            changelog_file = Path(DEFAULT_CHANGELOG_FILE)
+
+        if not changelog_file.exists():
+            raise ChangelogError(
+                f"Changelog file {changelog_file} does not exist."
+            )
+
+        # Try to get the unreleased section of the specific version
+        changelog_text, release_text = update_changelog(
+            changelog_file.read_text(encoding="utf-8"),
+            release_version,
+            git_tag_prefix=self.git_tag_prefix,
+            containing_version=release_version,
+        )
+
+        if not changelog_text:
+            # Try to get unversioned unrelease section
+            changelog_text, release_text = update_changelog(
+                changelog_file.read_text(encoding="utf-8"),
+                release_version,
+                git_tag_prefix=self.git_tag_prefix,
+            )
+
+        if not changelog_text:
+            raise ChangelogError(
+                f"No unreleased text found in {changelog_file}"
+            )
+
+        changelog_file.write_text(changelog_text, encoding="utf8")
+
+        self.git.add(changelog_file)
+
+        return release_text
+
+    def _create_changelog(self, release_version: str, cc_config: Path) -> str:
+        last_release_version = get_last_release_version()
+        changelog_builder = ChangelogBuilder(
+            terminal=self.terminal,
+            current_version=last_release_version,
+            next_version=release_version,
+            space=self.space,
+            project=self.project,
+            config=cc_config,
+        )
+
+        output_file = changelog_builder.create_changelog_file(
+            f"v{release_version}.md"
+        )
+        self.git.add(output_file)
+        return output_file.read_text(encoding="utf-8").replace(
+            "# Changelog\n\n"
+            "All notable changes to this project "
+            "will be documented in this file.\n\n",
+            "",
+        )
+
+    def run(
+        self,
+        *,
+        space: str,
+        project: Optional[str],
+        release_type: ReleaseType,
+        release_version: Optional[str],
+        git_signing_key: Optional[str],
+        git_tag_prefix: Optional[str],
+        changelog_file: Optional[Path],
+        conventional_commits: bool,
+        cc_config: Optional[Path],
+    ) -> PrepareReturnValue:
+        """
+        Prepare a release
+
+        Args:
+            space: GitHub username or organization. Required for generating
+                links in the changelog.
+            project: Name of the project to release. If not set it will be
+                gathered via the git remote url.
+            release_type: Type of the release to prepare. Defines the release
+                version. PATCH increments the bugfix version. CALENDAR creates
+                a new CalVer release version. VERSION uses the provided
+                release_version.
+            release_version: Release version to use if release type is VERSION.
+            git_signing_key: A GPG key ID to use for creating signatures.
+            git_tag_prefix: An optional prefix to use for creating a git tag
+                from the release version.
+            changelog_file: A changelog file to use for releases not using
+                conventional commits.
+            conventional_commits: Create changelog and release description from
+                conventional commits.
+            cc_config: A path to a settings file for creating conventional
+                commits.
+        """
+        self.git_tag_prefix = git_tag_prefix or ""
+        git_signing_key = (
+            git_signing_key
+            if git_signing_key is not None
+            else find_signing_key(self.terminal)
+        )
+        self.project = (
+            project if project is not None else get_git_repository_name()
+        )
+        self.space = space
+
+        try:
+            release_version = self._get_release_version(
+                release_type, release_version
+            )
+        except VersionError as e:
+            self.terminal.error(f"Unable to determine release version. {e}")
+            return PrepareReturnValue.NO_RELEASE_VERSION
+
+        if not release_version:
+            return PrepareReturnValue.NO_RELEASE_VERSION
+
+        self.terminal.info(f"Preparing the release {release_version}")
+
+        git_version = f"{self.git_tag_prefix}{release_version}"
+
+        if self._has_tag(git_version):
+            self.terminal.error(f"Git tag {git_version} already exists.")
+            return PrepareReturnValue.ALREADY_TAKEN
+
+        try:
+            updated = self._update_version(release_version)
+
+            self.terminal.ok(f"Updated version to {release_version}")
+
+            for path in updated.changed_files:
+                self.git.add(path)
+        except VersionError as e:
+            self.terminal.error(
+                f"Unable to update version to {release_version}. {e}"
+            )
+            return PrepareReturnValue.UPDATE_VERSION_ERROR
+
+        if conventional_commits:
+            release_text = self._create_changelog(release_version, cc_config)
+        else:
+            release_text = self._create_old_changelog(
+                release_version, changelog_file
+            )
+
+        self.terminal.info("Committing changes")
+
+        commit_msg = f"Automatic release to {release_version}"
+
+        self.git.commit(
+            commit_msg, verify=False, gpg_signing_key=git_signing_key
+        )
+        self.git.tag(
+            git_version, gpg_key_id=git_signing_key, message=commit_msg
+        )
+
+        release_text_file = Path(RELEASE_TEXT_FILE)
+        release_text_file.write_text(release_text, encoding="utf-8")
+
+        self.terminal.warning(
+            f"Please verify git tag {git_version}, "
+            f"commit and release text in {release_text_file.absolute()}"
+        )
+        self.terminal.print("Afterwards please execute release")
+
+        return PrepareReturnValue.SUCCESS
 
 
 def prepare(
@@ -49,129 +268,22 @@ def prepare(
     args: Namespace,
     **_kwargs,
 ) -> IntEnum:
-    git_tag_prefix: str = args.git_tag_prefix
-    git_signing_key: str = (
-        args.git_signing_key
-        if args.git_signing_key is not None
-        else find_signing_key(terminal)
-    )
-    project: str = (
-        args.project if args.project is not None else get_git_repository_name()
-    )
-    space: str = args.space
-    calendar: bool = args.calendar
-    patch: bool = args.patch
-
-    if calendar:
-        release_version: str = calculate_calendar_version(terminal)
-    elif patch:
-        release_version: str = get_next_patch_version(terminal)
+    cmd = PrepareCommand(terminal)
+    if args.calendar:
+        release_type = ReleaseType.CALENDAR
+    elif args.patch:
+        release_type = ReleaseType.PATCH
     else:
-        release_version: str = args.release_version
+        release_type = ReleaseType.VERSION
 
-    if not release_version:
-        return PrepareReturnValue.NO_RELEASE_VERSION
-
-    terminal.info(f"Preparing the release {release_version}")
-
-    # guardian
-    git = Git()
-    git_tags = git.list_tags()
-    git_version = f"{git_tag_prefix}{release_version}"
-    if git_version in git_tags:
-        terminal.error(f"git tag {git_version} is already taken.")
-        return PrepareReturnValue.ALREADY_TAKEN
-
-    executed, filename = update_version(terminal, release_version)
-    if not executed:
-        return PrepareReturnValue.UPDATE_VERSION_ERROR
-
-    terminal.ok(f"updated version in {filename} to {release_version}")
-
-    changelog_bool = True
-    if args.conventional_commits:
-        last_release_version = get_last_release_version()
-        changelog_builder = changelog.ChangelogBuilder(
-            terminal=terminal,
-            current_version=last_release_version,
-            next_version=release_version,
-            space=space,
-            project=project,
-            config=args.cc_config,
-        )
-
-        output_file = changelog_builder.create_changelog_file(
-            f"v{release_version}.md"
-        )
-        terminal.ok(f"Created changelog {output_file}")
-        commit_msg = f"Changelog created for release to {release_version}"
-        commit_files(
-            git,
-            output_file,
-            commit_msg,
-            git_signing_key=git_signing_key,
-        )
-        changelog_bool = False
-        # Remove the header for the release text
-        changelog_text = output_file.read_text(encoding="utf-8").replace(
-            "# Changelog\n\n"
-            "All notable changes to this project "
-            "will be documented in this file.\n\n",
-            "",
-        )
-    else:
-        change_log_path = Path.cwd() / "CHANGELOG.md"
-        if args.changelog:
-            tmp_path = Path.cwd() / Path(args.changelog)
-            if tmp_path.is_file():
-                change_log_path = tmp_path
-            else:
-                terminal.warning(f"{tmp_path} is not a file.")
-
-        # Try to get the unreleased section of the specific version
-        updated, changelog_text = changelog.update(
-            change_log_path.read_text(encoding="utf-8"),
-            release_version,
-            git_tag_prefix=git_tag_prefix,
-            containing_version=release_version,
-        )
-
-        if not updated:
-            # Try to get unversioned unrelease section
-            updated, changelog_text = changelog.update(
-                change_log_path.read_text(encoding="utf-8"),
-                release_version,
-                git_tag_prefix=git_tag_prefix,
-            )
-
-        if not updated:
-            terminal.error("No unreleased text found in CHANGELOG.md")
-            return PrepareReturnValue.NO_UNRELEASED_CHANGELOG
-
-        change_log_path.write_text(updated, encoding="utf-8")
-
-        terminal.ok("Updated CHANGELOG.md")
-
-    terminal.info("Committing changes")
-
-    commit_msg = f"Automatic release to {release_version}"
-    commit_files(
-        git,
-        filename,
-        commit_msg,
-        git_signing_key=git_signing_key,
-        changelog=changelog_bool,
+    return cmd.run(
+        git_signing_key=args.git_signing_key,
+        project=args.project,
+        space=args.space,
+        release_type=release_type,
+        release_version=args.release_version,
+        git_tag_prefix=args.git_tag_prefix,
+        changelog_file=args.changelog,
+        cc_config=args.cc_config,
+        conventional_commits=args.conventional_commits,
     )
-
-    git.tag(git_version, gpg_key_id=git_signing_key, message=commit_msg)
-
-    release_text = Path(RELEASE_TEXT_FILE)
-    release_text.write_text(changelog_text, encoding="utf-8")
-
-    terminal.warning(
-        f"Please verify git tag {git_version}, "
-        f"commit and release text in {str(release_text)}"
-    )
-    terminal.print("Afterwards please execute release")
-
-    return PrepareReturnValue.SUCCESS
