1e25b2f4211138366239027ec72d87f9ed5fca4c
Björn Ricks <bjoern.ricks@greenbone.net>
2023-02-21 15:09:22 +0100

    Change: Update release CLI for VersionCommand/Calculator changes
    Adjust release CLI to VersionCommand and VersionCalculator changes.

--- tests/release/test_prepare.py
+++ tests/release/test_prepare.py
@@ -17,46 +17,46 @@
 
 # pylint: disable=line-too-long
 
-import os
 import unittest
 from pathlib import Path
 from unittest.mock import MagicMock, call, patch
 
+from pontos.git.git import ConfigScope, Git
 from pontos.release.main import parse_args
 from pontos.release.prepare import PrepareReturnValue, prepare
 from pontos.terminal.terminal import Terminal
 from pontos.testing import temp_git_repository
+from pontos.version.calculator import VersionCalculator
 from pontos.version.errors import VersionError
-from pontos.version.helper import calculate_calendar_version
-from pontos.version.version import VersionUpdate
+from pontos.version.go import GoVersionCommand
+from pontos.version.version import VersionCommand, VersionUpdate
 
 
 def mock_terminal() -> MagicMock:
     return MagicMock(spec=Terminal)
 
 
+@patch.dict("os.environ", {"GITHUB_TOKEN": "foo", "GITHUB_USER": "user"})
 class PrepareTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        os.environ["GITHUB_TOKEN"] = "foo"
-        os.environ["GITHUB_USER"] = "bar"
-
     @patch("pontos.release.prepare.Git", autospec=True)
     @patch("pontos.release.prepare.find_signing_key", autospec=True)
-    @patch(
-        "pontos.release.prepare.PrepareCommand._update_version", autospec=True
-    )
     @patch(
         "pontos.release.prepare.PrepareCommand._create_changelog",
         autospec=True,
     )
-    def test_prepare_successfully(
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
+    def test_prepare_release_type_version(
         self,
+        gather_project_mock: MagicMock,
         create_changelog_mock: MagicMock,
-        update_version_mock: MagicMock,
         find_signing_key_mock: MagicMock,
         git_mock: MagicMock,
     ):
-        update_version_mock.return_value = VersionUpdate(
+        version_command_mock = MagicMock(spec=VersionCommand)
+        gather_project_mock.return_value = version_command_mock
+        version_command_mock.update_version.return_value = VersionUpdate(
             previous="0.0.0", new="0.0.1", changed_files=["MyProject.conf"]
         )
         create_changelog_mock.return_value = "A changelog text"
@@ -90,26 +90,28 @@ class PrepareTestCase(unittest.TestCase):
 
     @patch("pontos.release.prepare.Git", autospec=True)
     @patch("pontos.release.prepare.find_signing_key", autospec=True)
-    @patch("pontos.release.prepare.get_current_version", autospec=True)
-    @patch(
-        "pontos.release.prepare.PrepareCommand._update_version", autospec=True
-    )
     @patch(
         "pontos.release.prepare.PrepareCommand._create_changelog",
         autospec=True,
     )
-    def test_prepare_calendar_successfully(
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
+    def test_prepare_release_type_calendar(
         self,
+        gather_project_mock: MagicMock,
         create_changelog_mock: MagicMock,
-        update_version_mock: MagicMock,
-        get_current_version_mock: MagicMock,
         find_signing_key_mock: MagicMock,
         git_mock: MagicMock,
     ):
+        calculator = VersionCalculator()
         current_version = "21.6.0"
-        get_current_version_mock.return_value = current_version
-        calendar_version = calculate_calendar_version(current_version)
-        update_version_mock.return_value = VersionUpdate(
+        calendar_version = calculator.next_calendar_version(current_version)
+        version_command_mock = MagicMock(spec=VersionCommand)
+        version_command_mock.get_current_version.return_value = current_version
+        version_command_mock.get_version_calculator.return_value = calculator
+        gather_project_mock.return_value = version_command_mock
+        version_command_mock.update_version.return_value = VersionUpdate(
             previous=current_version,
             new=calendar_version,
             changed_files=["MyProject.conf"],
@@ -118,12 +120,7 @@ class PrepareTestCase(unittest.TestCase):
         find_signing_key_mock.return_value = "0815"
 
         _, _, args = parse_args(
-            [
-                "prepare",
-                "--project",
-                "foo",
-                "--calendar",
-            ]
+            ["prepare", "--project", "foo", "--release-type", "calendar"]
         )
         with temp_git_repository():
             released = prepare(
@@ -145,20 +142,75 @@ class PrepareTestCase(unittest.TestCase):
         )
 
     @patch("pontos.release.prepare.Git", autospec=True)
-    @patch(
-        "pontos.release.prepare.PrepareCommand._update_version", autospec=True
-    )
+    @patch("pontos.release.prepare.find_signing_key", autospec=True)
     @patch(
         "pontos.release.prepare.PrepareCommand._create_changelog",
         autospec=True,
     )
-    def test_use_git_signing_key_on_prepare(
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
+    def test_prepare_release_type_patch(
         self,
+        gather_project_mock: MagicMock,
         create_changelog_mock: MagicMock,
-        update_version_mock: MagicMock,
+        find_signing_key_mock: MagicMock,
         git_mock: MagicMock,
     ):
-        update_version_mock.return_value = VersionUpdate(
+        current_version = "21.6.0"
+        next_patch_version = "21.6.1"
+        calculator = VersionCalculator()
+        version_command_mock = MagicMock(spec=VersionCommand)
+        version_command_mock.get_current_version.return_value = current_version
+        version_command_mock.get_version_calculator.return_value = calculator
+        gather_project_mock.return_value = version_command_mock
+        version_command_mock.update_version.return_value = VersionUpdate(
+            previous=current_version,
+            new=next_patch_version,
+            changed_files=["MyProject.conf"],
+        )
+        create_changelog_mock.return_value = "A changelog text"
+        find_signing_key_mock.return_value = "0815"
+
+        _, _, args = parse_args(
+            ["prepare", "--project", "foo", "--release-type", "patch"]
+        )
+        with temp_git_repository():
+            released = prepare(
+                terminal=mock_terminal(),
+                args=args,
+            )
+        self.assertEqual(released, PrepareReturnValue.SUCCESS)
+
+        git_mock.return_value.add.assert_called_once_with("MyProject.conf")
+        git_mock.return_value.commit.assert_called_once_with(
+            f"Automatic release to {next_patch_version}",
+            verify=False,
+            gpg_signing_key="0815",
+        )
+        git_mock.return_value.tag.assert_called_once_with(
+            f"v{next_patch_version}",
+            message=f"Automatic release to {next_patch_version}",
+            gpg_key_id="0815",
+        )
+
+    @patch("pontos.release.prepare.Git", autospec=True)
+    @patch(
+        "pontos.release.prepare.PrepareCommand._create_changelog",
+        autospec=True,
+    )
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
+    def test_use_git_signing_key_on_prepare(
+        self,
+        gather_project_mock: MagicMock,
+        create_changelog_mock: MagicMock,
+        git_mock: MagicMock,
+    ):
+        version_command_mock = MagicMock(spec=VersionCommand)
+        gather_project_mock.return_value = version_command_mock
+        version_command_mock.update_version.return_value = VersionUpdate(
             previous="0.0.0", new="0.0.1", changed_files=["MyProject.conf"]
         )
         create_changelog_mock.return_value = "A changelog text"
@@ -191,10 +243,109 @@ class PrepareTestCase(unittest.TestCase):
         )
 
     @patch("pontos.release.prepare.Git", autospec=True)
+    def test_no_release_when_no_project_found(
+        self,
+        _git_mock: MagicMock,
+    ):
+        _, _, args = parse_args(
+            [
+                "prepare",
+                "--project",
+                "foo",
+                "--release-version",
+                "0.0.1",
+            ]
+        )
+        with temp_git_repository():
+            released = prepare(
+                terminal=mock_terminal(),
+                args=args,
+            )
+        self.assertEqual(
+            released, PrepareReturnValue.PROJECT_SETTINGS_NOT_FOUND
+        )
+
+    @patch("pontos.release.prepare.Git", autospec=True)
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
+    def test_no_release_version_error(
+        self,
+        gather_project_mock: MagicMock,
+        _git_mock: MagicMock,
+    ):
+        version_calculator_mock = MagicMock(spec=VersionCalculator)
+        version_calculator_mock.next_patch_version.side_effect = VersionError(
+            "An error"
+        )
+        version_command_mock = MagicMock(spec=VersionCommand)
+        version_command_mock.get_version_calculator.return_value = (
+            version_calculator_mock
+        )
+        gather_project_mock.return_value = version_command_mock
+
+        _, _, args = parse_args(
+            [
+                "prepare",
+                "--project",
+                "foo",
+                "--release-type",
+                "patch",
+            ]
+        )
+        with temp_git_repository():
+            released = prepare(
+                terminal=mock_terminal(),
+                args=args,
+            )
+
+        self.assertEqual(released, PrepareReturnValue.NO_RELEASE_VERSION)
+
+    @patch("pontos.release.prepare.Git", autospec=True)
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
+    def test_no_release_version(
+        self,
+        gather_project_mock: MagicMock,
+        _git_mock: MagicMock,
+    ):
+        version_calculator_mock = MagicMock(spec=VersionCalculator)
+        version_calculator_mock.next_patch_version.return_value = None
+        version_command_mock = MagicMock(spec=VersionCommand)
+        version_command_mock.get_version_calculator.return_value = (
+            version_calculator_mock
+        )
+        gather_project_mock.return_value = version_command_mock
+
+        _, _, args = parse_args(
+            [
+                "prepare",
+                "--project",
+                "foo",
+                "--release-type",
+                "patch",
+            ]
+        )
+        with temp_git_repository():
+            released = prepare(
+                terminal=mock_terminal(),
+                args=args,
+            )
+
+        self.assertEqual(released, PrepareReturnValue.NO_RELEASE_VERSION)
+
+    @patch("pontos.release.prepare.Git", autospec=True)
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
     def test_fail_if_tag_is_already_taken(
         self,
+        gather_project_mock: MagicMock,
         git_mock: MagicMock,
     ):
+        version_command_mock = MagicMock(spec=VersionCommand)
+        gather_project_mock.return_value = version_command_mock
         git_mock.return_value.list_tags.return_value = ["v0.0.1"]
 
         _, _, args = parse_args(
@@ -220,36 +371,17 @@ class PrepareTestCase(unittest.TestCase):
         git_mock.return_value.list_tags.assert_called_once()
 
     @patch("pontos.release.prepare.Git", autospec=True)
-    def test_no_release_when_no_project_found(
-        self,
-        _git_mock: MagicMock,
-    ):
-        _, _, args = parse_args(
-            [
-                "prepare",
-                "--project",
-                "foo",
-                "--release-version",
-                "0.0.1",
-            ]
-        )
-        with temp_git_repository():
-            released = prepare(
-                terminal=mock_terminal(),
-                args=args,
-            )
-        self.assertEqual(released, PrepareReturnValue.UPDATE_VERSION_ERROR)
-
-    @patch("pontos.release.prepare.Git", autospec=True)
     @patch(
-        "pontos.release.prepare.PrepareCommand._update_version", autospec=True
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
     )
-    def test_no_release_when_updating_version_fails(
+    def test_updating_version_error(
         self,
-        update_version_mock: MagicMock,
+        gather_project_mock: MagicMock,
         _git_mock: MagicMock,
     ):
-        update_version_mock.side_effect = VersionError(
+        version_command_mock = MagicMock(spec=VersionCommand)
+        gather_project_mock.return_value = version_command_mock
+        version_command_mock.update_version.side_effect = VersionError(
             "Error updating the version"
         )
 
@@ -270,17 +402,19 @@ class PrepareTestCase(unittest.TestCase):
         self.assertEqual(released, PrepareReturnValue.UPDATE_VERSION_ERROR)
 
     @patch("pontos.release.prepare.Git", autospec=True)
-    @patch(
-        "pontos.release.prepare.PrepareCommand._update_version", autospec=True
-    )
     @patch("pontos.release.prepare.ChangelogBuilder", autospec=True)
+    @patch(
+        "pontos.release.prepare.PrepareCommand._gather_project", autospec=True
+    )
     def test_prepare_conventional_commits(
         self,
+        gather_project_mock: MagicMock,
         changelog_builder_mock: MagicMock,
-        update_version_mock: MagicMock,
         git_mock: MagicMock,
     ):
-        update_version_mock.return_value = VersionUpdate(
+        version_command_mock = MagicMock(spec=VersionCommand)
+        gather_project_mock.return_value = version_command_mock
+        version_command_mock.update_version.return_value = VersionUpdate(
             previous="0.0.0", new="0.0.1", changed_files=["MyProject.conf"]
         )
 
@@ -326,4 +460,51 @@ class PrepareTestCase(unittest.TestCase):
             self.assertEqual(
                 release_file.read_text(encoding="utf-8"),
                 expected_release_content,
+            )
+
+    @patch("pontos.release.prepare.Git", autospec=True)
+    @patch(
+        "pontos.release.prepare.PrepareCommand._create_changelog",
+        autospec=True,
+    )
+    def test_prepare(
+        self,
+        create_changelog_mock: MagicMock,
+        git_mock: MagicMock,
+    ):
+        current_version = "21.6.0"
+        next_patch_version = "21.6.1"
+        gpg_key_id = "0815"
+        create_changelog_mock.return_value = "A changelog text"
+
+        _, _, args = parse_args(["prepare", "--release-type", "patch"])
+        with temp_git_repository() as temp_dir:
+            git = Git()
+            git.config("user.signingkey", gpg_key_id, scope=ConfigScope.LOCAL)
+            git.add_remote("origin", "http://foo/bar.git")
+
+            go_mod = temp_dir / GoVersionCommand.project_file_name
+            go_mod.touch()
+            version_file = temp_dir / GoVersionCommand.version_file_path
+            version_file.write_text(
+                f'var version = "{current_version}"', encoding="utf8"
+            )
+            released = prepare(
+                terminal=mock_terminal(),
+                args=args,
+            )
+
+            self.assertEqual(released, PrepareReturnValue.SUCCESS)
+
+            git_mock.return_value.add.assert_called_with(
+                GoVersionCommand.version_file_path
+            )
+            git_mock.return_value.commit.assert_called_with(
+                f"Automatic release to {next_patch_version}",
+                verify=False,
+                gpg_signing_key=gpg_key_id,
+            )
+
+            self.assertTrue(
+                next_patch_version in version_file.read_text(encoding="utf8")
             )
