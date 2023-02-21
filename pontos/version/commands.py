f5cb533abe211f0c00ca95f7a9c48cc5ad8a7044
Björn Ricks <bjoern.ricks@greenbone.net>
2023-02-21 15:07:49 +0100

    Remove: Drop get_current_version and update_version functions
    These functions have been wrappers around the commands only. The release
    CLI will use the commands directly.

--- pontos/version/commands.py
+++ pontos/version/commands.py
@@ -15,14 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Optional, Tuple, Type
+from typing import Tuple, Type
 
 from .cmake import CMakeVersionCommand
-from .errors import VersionError
 from .go import GoVersionCommand
 from .javascript import JavaScriptVersionCommand
 from .python import PythonVersionCommand
-from .version import VersionCommand, VersionUpdate
+from .version import VersionCommand
 
 COMMANDS: Tuple[Type[VersionCommand]] = (
     CMakeVersionCommand,
@@ -30,45 +29,3 @@ COMMANDS: Tuple[Type[VersionCommand]] = (
     JavaScriptVersionCommand,
     PythonVersionCommand,
 )
-
-
-def get_current_version() -> str:
-    """
-    Get the current Version for the current project
-
-    Raises:
-        VersionError if current version can not be found.
-    """
-
-    for cmd in COMMANDS:
-        command = cmd()
-        if command.project_found():
-            return command.get_current_version()
-
-    raise VersionError("No project settings file found")
-
-
-def update_version(
-    new_version: str, *, develop: Optional[bool] = False
-) -> VersionUpdate:
-    """
-    Update the Version for the current project in the relevant files
-
-    Args:
-        new_version: The version that will be set for the project
-        develop: Add a dev version section to the version string
-
-    Raises:
-        VersionError if no project settings (file) can be found.
-
-    Returns:
-        Information about the version update
-    """
-    for cmd in COMMANDS:
-        command = cmd()
-        if command.project_found():
-            return command.update_version(
-                new_version=new_version, develop=develop
-            )
-
-    raise VersionError("No project settings file found")
