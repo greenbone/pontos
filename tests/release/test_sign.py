e5cd575fd63fc5f29482b11e2bd9b88deb117721
Björn Ricks <bjoern.ricks@greenbone.net>
2023-02-28 13:13:44 +0100

    Change: Update pontos-release sign to use last git tag as release version
    Instead of using the current version determined by a version command as
    the release version use the last git tag. This ensures that sign can be
    used in the CI after the release without having to determine the correct
    release version manually.

--- tests/release/test_sign.py
+++ tests/release/test_sign.py
@@ -71,7 +71,28 @@ class SignTestCase(unittest.TestCase):
 
             self.assertEqual(result, SignReturnValue.NO_PROJECT)
 
-    def test_no_project_setup(self):
+    def test_no_release_error(self):
+        with temp_directory(change_into=True):
+            _, token, args = parse_args(
+                [
+                    "sign",
+                    "--project",
+                    "foo",
+                ]
+            )
+
+            result = sign(
+                terminal=mock_terminal(),
+                args=args,
+                token=token,
+            )
+
+            self.assertEqual(result, SignReturnValue.NO_RELEASE_VERSION)
+
+    @patch("pontos.release.sign.get_last_release_version", autospec=True)
+    def test_no_release_version(self, get_last_release_version_mock: MagicMock):
+        get_last_release_version_mock.return_value = None
+
         with temp_directory(change_into=True):
             _, token, args = parse_args(
                 [
@@ -156,6 +177,114 @@ class SignTestCase(unittest.TestCase):
                     "foo",
                     "--release-version",
                     "1.2.3",
+                ]
+            )
+
+            result = sign(
+                terminal=mock_terminal(),
+                args=args,
+                token=token,
+            )
+
+            self.assertEqual(result, SignReturnValue.SUCCESS)
+
+            cmd_runner_mock.assert_has_calls(
+                [
+                    call(
+                        "gpg",
+                        "--default-key",
+                        "0ED1E580",
+                        "--yes",
+                        "--detach-sign",
+                        "--armor",
+                        zip_file,
+                    ),
+                    call(
+                        "gpg",
+                        "--default-key",
+                        "0ED1E580",
+                        "--yes",
+                        "--detach-sign",
+                        "--armor",
+                        tar_file,
+                    ),
+                    call(
+                        "gpg",
+                        "--default-key",
+                        "0ED1E580",
+                        "--yes",
+                        "--detach-sign",
+                        "--armor",
+                        some_asset,
+                    ),
+                    call(
+                        "gpg",
+                        "--default-key",
+                        "0ED1E580",
+                        "--yes",
+                        "--detach-sign",
+                        "--armor",
+                        other_asset,
+                    ),
+                ]
+            )
+
+            github_releases_mock.upload_release_assets.assert_called_once_with(
+                "greenbone/foo",
+                "v1.2.3",
+                [
+                    (Path("file.zip.asc"), "application/pgp-signature"),
+                    (Path("file.tar.asc"), "application/pgp-signature"),
+                    (Path("file1.asc"), "application/pgp-signature"),
+                    (Path("file2.asc"), "application/pgp-signature"),
+                ],
+            )
+
+    @patch("pontos.release.helper.Git", autospec=True)
+    @patch("pontos.release.sign.cmd_runner", autospec=True)
+    @patch("pontos.release.sign.SignCommand.download_asset", autospec=True)
+    @patch("pontos.release.sign.SignCommand.download_tar", autospec=True)
+    @patch("pontos.release.sign.SignCommand.download_zip", autospec=True)
+    @patch("pontos.release.sign.GitHubAsyncRESTApi.releases", autospec=True)
+    def test_sign_success_determine_release_version(
+        self,
+        github_releases_mock: AsyncMock,
+        download_zip_mock: AsyncMock,
+        download_tar_mock: AsyncMock,
+        download_asset_mock: AsyncMock,
+        cmd_runner_mock: AsyncMock,
+        git_mock: MagicMock,
+    ):
+        tar_file = Path("file.tar")
+        zip_file = Path("file.zip")
+        some_asset = Path("file1")
+        other_asset = Path("file2")
+        download_tar_mock.return_value = tar_file
+        download_zip_mock.return_value = zip_file
+        download_asset_mock.side_effect = [some_asset, other_asset]
+        github_releases_mock.exists = AsyncMock(return_value=True)
+        github_releases_mock.download_release_assets.return_value = (
+            AsyncIteratorMock(
+                [
+                    (
+                        "foo",
+                        MagicMock(),
+                    ),
+                    ("bar", MagicMock()),
+                ]
+            )
+        )
+        process = AsyncMock(spec=Process, returncode=0)
+        process.communicate.return_value = ("", "")
+        cmd_runner_mock.return_value = process
+        git_mock.return_value.list_tags.return_value = ["v1.0.0", "v1.2.3"]
+
+        with temp_directory(change_into=True):
+            _, token, args = parse_args(
+                [
+                    "sign",
+                    "--project",
+                    "foo",
                 ]
             )
 
