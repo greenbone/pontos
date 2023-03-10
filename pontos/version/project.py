b2bf4990ad43487447d67240e2fbf4b780863abb
Björn Ricks <bjoern.ricks@greenbone.net>
2023-03-10 10:58:17 +0100

    Change: Refactor Project class
    * Drop static gather_project method in favor of using the constructor
      directly
    * Require a VersioningScheme to be passed to the constructor

--- pontos/version/project.py
+++ pontos/version/project.py
@@ -18,38 +18,73 @@
 
 from typing import List, Literal, Union
 
-from pontos.version.commands import get_commands
-from pontos.version.errors import ProjectError
-from pontos.version.version import Version, VersionCommand, VersionUpdate
+from .commands import VersionCommand, get_commands
+from .errors import ProjectError
+from .schemes import VersioningScheme
+from .version import Version, VersionUpdate
+
+__all__ = ("Project",)
 
 
 class Project:
-    def __init__(self, commands: List[VersionCommand]) -> None:
-        self._commands = commands
+    """
+    A project for handling versioning
 
-    @classmethod
-    def gather_project(cls) -> "Project":
+    Example:
+        .. code-block:: python
+
+            from pontos.version.scheme import PEP440VersioningScheme
+            from pontos.version.project import Project
+
+            project = Project(PEP440VersioningScheme)
+    """
+
+    def __init__(self, versioning_scheme: VersioningScheme) -> None:
         """
-        Get the project with the fitting VersionCommands of the current working
-        directory
+        Creates a new project instance
+
+        Args:
+            versioning_scheme: Scheme for version handling
 
         Raises:
-            ProjectError if no fitting VersionCommand could be found
+            ProjectError: If no fitting VersionCommand could be found
+        """
+        self._versioning_scheme = versioning_scheme
+        self._commands = self._gather_commands()
+
+    def _gather_commands(self) -> List[VersionCommand]:
+        """
+        Initialize the project with the fitting VersionCommands of the current
+        working directory
+
+        Raises:
+            ProjectError: If no fitting VersionCommand could be found
         """
         commands = []
         for cmd in get_commands():
-            command = cmd()
+            command = cmd(versioning_scheme=self._versioning_scheme)
             if command.project_found():
                 commands.append(command)
 
         if not commands:
             raise ProjectError("No project settings file found")
 
-        return cls(commands)
+        return commands
 
     def update_version(
         self, new_version: Version, *, force: bool = False
     ) -> VersionUpdate:
+        """
+        Update the current version of this project
+
+        Args:
+            new_version: Use this version in the update
+            force: Force updating the version even if the current version is the
+                same as the new version
+
+        Returns:
+            The version update including the changed files
+        """
         update = self._commands[0].update_version(new_version, force=force)
         for cmd in self._commands[1:]:
             next_update = cmd.update_version(new_version, force=force)
@@ -58,10 +93,25 @@ class Project:
         return update
 
     def get_current_version(self) -> Version:
+        """
+        Get the current version of the project
+
+        Returns:
+            The current version
+        """
         return self._commands[0].get_current_version()
 
     def verify_version(
         self, version: Union[Literal["current"], Version]
     ) -> None:
+        """
+        Verify the current version of this project
+
+        Args:
+            version: Version to check against the current applied version of
+                this project. If version is "current" the command should verify
+                if all version information is consistent, for example if the
+                version information in several files is the same.
+        """
         for cmd in self._commands:
             cmd.verify_version(version)
