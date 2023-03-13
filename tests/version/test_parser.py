8064b87f6316ce80369fe8bc9e516cfebabbc848
Jaspar Stach <jaspar.stach@greenbone.net>
2023-03-13 20:50:53 +0100

    Add parser test

---
+++ tests/version/test_parser.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2023 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from pontos.version.parser import parse_args
+
+
+class ParserTestCase(unittest.TestCase):
+    def test_error_while_parsing(self):
+        with self.assertRaises(SystemExit) as cm:
+            parse_args(["update", "foo"])
+
+        # exception code in argparse
+        self.assertEqual(cm.exception.code, 2)
