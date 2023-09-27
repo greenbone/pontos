d8add03dcd8dac043671b4a702fdf0389b5b930c
Stefan Tolksdorf <stefan.tolksdorf@greenbone.net>
2023-09-27 18:26:56 +0200

    Add test for get_current_version

--- tests/version/commands/test_java.py
+++ tests/version/commands/test_java.py
@@ -14,6 +14,47 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import unittest
+from pathlib import Path
 
-# pylint: disable=line-too-long
-# ruff: noqa: E501
+from pontos.testing import temp_file
+from pontos.version.commands import JavaVersionCommand
+from pontos.version.schemes import SemanticVersioningScheme
+
+TEMPLATE_UPGRADE_VERSION_JSON = """{
+  "files": [
+    {
+      "path": "README.md",
+      "line": 3
+    },
+  ]
+}
+"""
+
+TEMPLATE_UPGRADE_VERSION_MARKDOWN = """# Task service
+
+**task service**: Version 2023.9.3
+
+## starting the local 
+"""
+
+
+class GetCurrentJavaVersionCommandTestCase(unittest.TestCase):
+    def test_getting_version(self):
+        with temp_file(
+            name="go.mod",
+            change_into=True,
+        ):
+            version_file_path = Path("upgradeVersion.json")
+            version_file_path.write_text(
+                TEMPLATE_UPGRADE_VERSION_JSON, encoding="utf-8"
+            )
+
+            result_version = JavaVersionCommand(
+                SemanticVersioningScheme
+            ).get_current_version()
+
+            self.assertEqual(
+                result_version, SemanticVersioningScheme.parse_version("2023.9.3")
+            )
+            version_file_path.unlink()
