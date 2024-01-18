42ff0716cddd5bf9895112f263f64147247f6e83
Björn Ricks <bjoern.ricks@greenbone.net>
2024-01-18 13:49:21 +0100

    Add: Export all relevant git classes from `pontos.git`
    All git related classes should be imported from `pontos.git`. Other
    classes, modules, functions, variables, ... not imported into
    `pontos.git` are considered private.

--- pontos/git/__init__.py
+++ pontos/git/__init__.py
@@ -3,12 +3,23 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-from .git import ConfigScope, Git, GitError, MergeStrategy, TagSort
+from .git import (
+    DEFAULT_TAG_SORT_SUFFIX,
+    ConfigScope,
+    Git,
+    GitError,
+    MergeStrategy,
+    TagSort,
+)
+from .status import Status, StatusEntry
 
 __all__ = (
-    "MergeStrategy",
+    "DEFAULT_TAG_SORT_SUFFIX",
     "ConfigScope",
-    "TagSort",
-    "GitError",
     "Git",
+    "GitError",
+    "MergeStrategy",
+    "Status",
+    "StatusEntry",
+    "TagSort",
 )
