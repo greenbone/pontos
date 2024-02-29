c7b42417f8e2845171bd0fa8af7a40eb4c4db837
Jaspar Stach <jaspar.stach@greenbone.net>
2024-02-29 20:11:03 +0100

    Add: Make`enum_choice` and `enum_type` accessible for other projects

--- pontos/__init__.py
+++ pontos/__init__.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+from .enum import enum_choice, enum_type
 from .pontos import main
 
-__all__ = ["main"]
+__all__ = ["enum_choice", "enum_type", "main"]
