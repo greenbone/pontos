5939e74637428ca44e730ddc4244310e454647a0
Gabriel Schowe <gabriel.schowe@greenbone.net>
2024-09-19 00:29:35 +0200

    Fix: Test had wront wait

--- tests/github/api/test_packages.py
+++ tests/github/api/test_packages.py
@@ -6,10 +6,10 @@
 from pontos.github.api.packages import GitHubAsyncRESTPackages
 from pontos.github.models.packages import (
     Package,
-    PackageType,
     PackageVersion,
     PackageVisibility,
 )
+from pontos.github.models.packages import PackageType
 from pontos.testing import AsyncIteratorMock
 from tests import aiter, anext
 from tests.github.api import GitHubAsyncRESTTestCase, create_response
@@ -251,9 +251,9 @@ class GitHubAsyncRESTPackagesTestCase(GitHubAsyncRESTTestCase):
             tag="latest",
         )
 
-        self.client.delete.assert_any_await(
+        self.client.delete.assert_awaited_once_with(
             "/orgs/foo/packages/container/bar/versions/1"
         )
-        self.client.delete.assert_any_await(
+        self.client.delete.assert_awaited_once_with(
             "/orgs/foo/packages/container/bar/versions/tags/latest"
         )
