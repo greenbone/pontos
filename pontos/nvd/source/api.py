880a893b4587d2a432e992177e165ff3c25d3d8b
Nicolas Thumann <nicolas.thumann@greenbone.net>
2025-02-21 15:35:26 +0100

    Change: Fix copyright headers

--- pontos/nvd/source/api.py
+++ pontos/nvd/source/api.py
@@ -1,7 +1,6 @@
-# SPDX-FileCopyrightText: 2022-2023 Greenbone AG
+# SPDX-FileCopyrightText: 2022-2025 Greenbone AG
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-#
 
 from datetime import datetime
 from typing import (
