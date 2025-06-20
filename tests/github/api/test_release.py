5d35d92e07c8112451a49e23bb3d10a0b53e062e
Nicolas Thumann <nicolas.thumann@greenbone.net>
2025-06-20 09:15:55 +0200

    Change: Adjust unit tests

--- tests/github/api/test_release.py
+++ tests/github/api/test_release.py
@@ -107,7 +107,7 @@ class GitHubAsyncRESTReleasesTestCase(GitHubAsyncRESTTestCase):
         )
 
     async def test_not_exists(self):
-        response = create_response(is_success=False)
+        response = create_response(is_success=False, status_code=404)
         self.client.get.return_value = response
 
         self.assertFalse(await self.api.exists("foo/bar", "v1.2.3"))
