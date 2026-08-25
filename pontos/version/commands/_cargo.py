0397311a1740676feb4f935b1f1a96e5db2a8278
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-25 11:08:52 +0200

    Add: Also update `Cargo.lock` file during version updates
    The `Cargo.lock` file also contains the version of the current package.
    Therefore during a version update for example in a release the lock file
    needs to be updated too.

--- pontos/version/commands/_cargo.py
+++ pontos/version/commands/_cargo.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from pathlib import Path
-from typing import Any, Literal
+from typing import Literal
 
 import tomlkit
 from tomlkit.toml_document import TOMLDocument
@@ -15,52 +15,116 @@ from ._command import VersionCommand
 
 class CargoVersionCommand(VersionCommand):
     project_file_name = "Cargo.toml"
+    cargo_lock_file_name = "Cargo.lock"
+    _cargo_lock_file_path: Path | None = None
 
-    def __has_package_version(self, toml: TOMLDocument):
+    @property
+    def cargo_toml(self) -> tomlkit.TOMLDocument:
+        if not self.project_file_path.exists():
+            raise VersionError(f"{self.project_file_name} file not found.")
+
+        return tomlkit.parse(self.project_file_path.read_text(encoding="utf-8"))
+
+    @property
+    def cargo_lock_file_path(self) -> Path:
+        if self._cargo_lock_file_path:
+            return self._cargo_lock_file_path
+
+        self._cargo_lock_file_path = (
+            self.project_file_path.parent / self.cargo_lock_file_name
+        )
+        return self._cargo_lock_file_path
+
+    def _has_package_version(self, toml: TOMLDocument):
         """
         Checks if the 'package' table contains a 'version'.
         """
-        # get a pure python object (recursively)
-        toml_dict = toml.unwrap()
-        return "package" in toml_dict and "version" in toml_dict["package"]
+        return "package" in toml and "version" in toml["package"]
 
-    def __has_workspace_package_version(self, toml: TOMLDocument):
+    def _has_workspace_package_version(self, toml: TOMLDocument):
         """
         Checks if the 'workspace.package' table contains a 'version'.
         """
-        # get a pure python object (recursively)
-        toml_dict = toml.unwrap()
         return (
-            "workspace" in toml_dict
-            and "package" in toml_dict["workspace"]
-            and "version" in toml_dict["workspace"]["package"]
+            "workspace" in toml
+            and "package" in toml["workspace"]
+            and "version" in toml["workspace"]["package"]
         )
 
-    def __as_project_document(
-        self, origin: Path
-    ) -> tuple[Path, tomlkit.TOMLDocument]:
-        """
-        Parse the given origin and returns a tuple of path to a
-        Cargo.toml that contains a version.
-        Version should be in the table package or workspace.package
-
-        If the origin is invalid toml than it will raise a VersionError.
-        """
-        version: Any = None
-        content = origin.read_text(encoding="utf-8")
-        content = tomlkit.parse(content)
-        if self.__has_workspace_package_version(content):
-            version = content.get("workspace").get("package").get("version")  # type: ignore[union-attr]
-
-        if self.__has_package_version(content):
-            version = content.get("package").get("version")  # type: ignore[union-attr]
-
-        if version:
-            return (origin, content)
-        else:
-            raise VersionError(
-                f"No {origin} file found. This file is required for pontos."
+    def _get_version_from_cargo_toml(
+        self,
+    ) -> Version:
+        """Get the current version from the Cargo.toml file."""
+        cargo_toml = self.cargo_toml
+        version: str | None = None
+        if self._has_workspace_package_version(cargo_toml):
+            version = (
+                cargo_toml.get("workspace").get("package").get("version")  # type: ignore
             )
+
+        if self._has_package_version(cargo_toml):
+            version = cargo_toml.get("package").get("version")  # type: ignore
+
+        if version is not None:
+            return self.versioning_scheme.parse_version(version)
+
+        raise VersionError(
+            f"No version information in {self.project_file_path} file found. This file is required for pontos."
+        )
+
+    def _update_cargo_toml_file(self, new_version: Version) -> None:
+        """
+        Update the Cargo.toml file with the new version.
+        """
+        cargo_toml = self.cargo_toml
+        if self._has_workspace_package_version(cargo_toml):
+            cargo_toml["workspace"]["package"]["version"] = str(new_version)
+        else:
+            # ensure the [package] section exists
+            if "package" not in cargo_toml:
+                package_table = tomlkit.table()
+                cargo_toml["package"] = package_table
+                cargo_toml["package"]["version"] = str(new_version)
+
+        if self._has_package_version(cargo_toml):
+            cargo_toml["package"]["version"] = str(new_version)
+
+        self.project_file_path.write_text(
+            tomlkit.dumps(cargo_toml), encoding="utf-8"
+        )
+
+    def _update_cargo_lock_file(self, new_version: Version) -> None:
+        """
+        Update the Cargo.lock file with the new version.
+        """
+        if not self.cargo_lock_file_path.exists():
+            raise VersionError(
+                f"{self.cargo_lock_file_name} file not found. Cannot update version."
+            )
+
+        cargo_lock = tomlkit.parse(
+            self.cargo_lock_file_path.read_text(encoding="utf-8")
+        )
+
+        if "package" not in cargo_lock:
+            raise VersionError(
+                f"[[package]] entries not found in {self.cargo_lock_file_path}. "
+                "Cannot update version."
+            )
+        project_name = self.cargo_toml.get("package", {}).get("name")
+        if project_name is None:
+            raise VersionError(
+                f"Project name not found in {self.project_file_path}. Cannot update version."
+            )
+
+        if "package" in cargo_lock:
+            for package in cargo_lock["package"]:
+                if package.get("name") == project_name:
+                    package["version"] = str(new_version)
+
+        self.cargo_lock_file_path.write_text(
+            tomlkit.dumps(cargo_lock), encoding="utf-8"
+        )
 
     def update_version(
         self, new_version: Version, *, force: bool = False
@@ -73,44 +137,28 @@ class CargoVersionCommand(VersionCommand):
             # just ignore current version and override it
             previous_version = None
 
-        project_path, project = self.__as_project_document(
-            self.project_file_path
-        )
+        try:
+            self._update_cargo_toml_file(new_version=new_version)
+        except OSError as e:
+            raise VersionError(
+                f"Unable to update version in {self.project_file_path.absolute()}. Error was {e}"
+            ) from e
 
-        if self.__has_workspace_package_version(project):
-            # Set the version for all members of the workspace. Members of the
-            # workspace should use `version.workspace=true` in the 'package' table,
-            # if they are released together with the parent crate.
-            project["workspace"]["package"]["version"] = str(new_version)  # type: ignore[index]
+        try:
+            self._update_cargo_lock_file(new_version=new_version)
+        except OSError as e:
+            raise VersionError(
+                f"Unable to update version in {self.cargo_lock_file_path.absolute()}. Error was {e}"
+            ) from e
 
-        if self.__has_package_version(project):
-            # Set the 'version' of the 'package' table for the parent crate
-            project["package"]["version"] = str(new_version)  # type: ignore[index]
-
-        project_path.write_text(tomlkit.dumps(project))
         return VersionUpdate(
             previous=previous_version,
             new=new_version,
-            changed_files=[project_path],
+            changed_files=[self.project_file_path],
         )
 
     def get_current_version(self) -> Version:
-        _, document = self.__as_project_document(self.project_file_path)
-
-        version: Any = None
-        if self.__has_workspace_package_version(document):
-            version = document["workspace"]["package"]["version"]  # type: ignore[index, arg-type]
-
-        if self.__has_package_version(document):
-            # If the 'package' table has a 'version', it always has precedence over the
-            # 'version' in the 'workspace.package' table (they are assumed to be equal, if
-            # managed by pontos)
-            version = document["package"]["version"]  # type: ignore[index, arg-type]
-
-        if isinstance(version, str):
-            current_version = self.versioning_scheme.parse_version(version)
-
-        return self.versioning_scheme.from_version(current_version)
+        return self._get_version_from_cargo_toml()
 
     def verify_version(
         self, version: Literal["current"] | Version | None
